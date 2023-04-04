ede68e68b8fc75aa56f30df215eda8c0a66a7091
woojunhee <129471977+woojunhee@users.noreply.github.com>
2023-04-04 12:55:01 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// 메뉴 네비게이션 기능
+$(function () {
+  var menu = $(".nav > li");
+  var contents = $("#wrap > div");
+
+  $(".nav> li").click(function (event) {
+    event.preventDefault();
+
+    var tg = $(this);
+    var i = tg.index();
+
+    var section = $("#wrap > div").eq(i);
+    var tt = section.offset().top;
+
+    $("html, body").stop().animate({ scrollTop: tt });
+  });
+  //메뉴에 addclass를 적용
+  // $(window).scroll(function(){
+  //     var sct = $(window).scrollTop();
+
+  //     $('#wrap > div').each(function(){
+  //         var tg = $(this);
+  //         var i = tg.index();
+  //         if(tg.offset().top <= sct){
+  //             $('.nav > li').removeClass('on');
+  //             $('.nav > li').eq(i).addClass('on');
+  //     }
+  // });
+  // $(window).scroll(function(){
+  // var sct = $(window).scrollTop();
+
+  // contents.each(function(){
+  //     var tg = $(this);
+  //     var i = tg.index();
+  //     if(tg.offset().top <= sct){
+  //         menu.removeClass('on');
+  //         menu.eq(i).addClass('on');
+  //     }
+  // });
+});
+
+
+
+// 퀵메뉴 기능
+$(function () {
+  $(window).scroll(
+      function () {
+          var windowTop = $(window).scrollTop() + 300;
+          $("#q_mn").stop().animate({ top: windowTop + "px" }, 500);
+          //$("#q_mn").stop().animate({속성:"속성값", 속성:"속성값" },1000);	
+      }
+  );
+});
